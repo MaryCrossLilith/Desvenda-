276a5f602fbaf80156d638815303ebc11f135193
Maria Eduarda Hurla <maria.hurla.oliveira@escola.pr.gov.br>
2026-07-14 22:46:01 +0000

    tirando o 1

--- script.js
+++ script.js
@@ -206,7 +206,7 @@ function carregarCarta() {
     // Alimenta a pergunta e altera o caminho da imagem correspondente
     elNumeroCarta.textContent = cartaAtual.id;
     elTextoPergunta.textContent = cartaAtual.texto;
-    elImgPergunta.src = `imagens1/${cartaAtual.id}_pergunta.png`;
+    elImgPergunta.src = `imagens/${cartaAtual.id}_pergunta.png`;
     
     // Alimenta a carta de resposta com a frente e verso do ID atual
     elImgFrente.src = `imagens/${cartaAtual.id}_frente.png`;
