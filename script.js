0c45bfe6008a7f82807ac8f20ef9d525379debb6
Maria Eduarda Hurla <maria.hurla.oliveira@escola.pr.gov.br>
2026-07-14 11:07:32 -0300

    arrumando o tempo para mostrar a explicação para 3seg

--- script.js
+++ script.js
@@ -244,7 +244,7 @@ window.verificarResposta = function(escolha) {
     }
 
     // Aguarda 5 segundos mostrando a resposta e então abre o modal explicativo
-    setTimeout(mostrarExplicacao, 5000);
+    setTimeout(mostrarExplicacao, 3000);
 }
 
 function mostrarFeedback(acertou) {
